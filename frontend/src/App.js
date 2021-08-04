5ce1f747c102a6e65df03d2f573febf81c6447d4
John <jbradysd@icloud.com>
2021-08-04 13:58:04 -0700

    adding header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to the shop</h1>
+      <Header />
+      <main>
+        <h1>Welcome to the shop</h1>
+      </main>
+      <Footer />
     </>
   )
 }

7dd67870a081ac8f94c5c84dba6a646aa2b6533a
John <jbradysd@icloud.com>
2021-08-04 18:38:04 -0700

    added rating component, fixed unique id error on homepage

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -16,9 +17,10 @@ const Product = ({ product }) => {
           </a>
 
           <Card.Text as='div'>
-            <div className='my-3'>
-              {product.rating} from {product.numReview} reviews
-            </div>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </Card.Text>
           <Card.Text as='h3'>${product.price}</Card.Text>
         </Card.Body>
